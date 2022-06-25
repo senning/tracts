bc7b13142534fd922f6c751651feab3c47bebc2f
Senning Luk <senning@puppydogtales.ca>
2022-06-25 18:59:09 -0400

    feat: add endpoints

---
+++ backend/main.py
@@ -0,0 +1,72 @@
+from flask import (
+    Flask,
+    abort,
+)
+import sqlite3
+
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect("tracts.gpkg")
+    return conn
+
+@app.errorhandler(404)
+def resource_not_found(e):
+  return {}, 404
+
+@app.route("/tracts")
+def list_tracts():
+    columns = ['fid', 'state', 'county', 'name','areaLand','areaWater','lat','lon']
+    conn = get_db_connection()
+    tracts = conn.execute('''
+    SELECT 
+        fid,
+        STATEFP,
+        COUNTYFP,
+        NAMELSAD,
+        ALAND,
+        AWATER,
+        INTPTLAT,
+        INTPTLON
+    FROM
+        tracts
+    ''').fetchall()
+    conn.close()
+
+    results = []
+    for row in tracts:
+        results.append(dict(zip(columns, row)))
+    return {
+        "objects": results
+    }
+
+
+@app.route("/tracts/<int:fid>")
+def get_tract(fid):
+    columns = ['fid', 'state', 'county', 'name','areaLand','areaWater','lat','lon']
+    conn = get_db_connection()
+    tract = conn.execute(f'''
+    SELECT 
+        fid,
+        STATEFP,
+        COUNTYFP,
+        NAMELSAD,
+        ALAND,
+        AWATER,
+        INTPTLAT,
+        INTPTLON
+    FROM
+        tracts
+    WHERE
+        fid = {fid}
+    ''').fetchone()
+    conn.close()
+    
+    if tract is None:
+      abort(404)
+
+    result = dict(zip(columns, tract))
+    return {
+        "data": result,
+    }
